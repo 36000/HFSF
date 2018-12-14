64e5574f9057bf548aca0c242563acb6371ec084
balbok0 <jakubflpk@gmail.com>
2018-12-14 09:17:35 -0800

    Mass plots added

--- funstuff.py
+++ funstuff.py
@@ -1,27 +1,44 @@
-import numpy as np 
-from data import getReadyData, getRawData
+import numpy as np
+from data import getReadyData
 from gen_plots import lundPlot
 import matplotlib.pyplot as plt
 
+
 def mass_get():
-    X_train, _, _, _, _, _ = getReadyData()
+    x_train, _, _, _, _, _ = getReadyData(include_mass=True)
+
     tp = np.load('true_pos.npy')
     fp = np.load('false_pos.npy')
     fn = np.load('false_neg.npy')
     tn = np.load('true_neg.npy')
 
-    tp_mass = X_train[tp, 0, 3]
-    fp_mass = X_train[fp, 0, 3]
-    fn_mass = X_train[fn, 0, 3]
-    tn_mass = X_train[tn, 0, 3]
+    tp_mass = x_train[tp, 0, 3]
+    fp_mass = x_train[fp, 0, 3]
+    fn_mass = x_train[fn, 0, 3]
+    tn_mass = x_train[tn, 0, 3]
 
-    
+    bins = np.linspace(0, 250, 500)
+
+    fig_1 = plt.subplot(2, 1, 1)
+    fig_1.hist(tp_mass, label='Mass W c.a. W', color='orange', histtype='step', bins=bins)
+    fig_1.hist(fp_mass, label='Mass QCD c.a. W', color='blue', histtype='step', bins=bins)
+    fig_1.legend()
+
+    fig_2 = plt.subplot(2, 1, 2)
+    fig_2.hist(tn_mass, label='Mass QCD c.a. QCD', color='blue', histtype='step', bins=bins)
+    fig_2.hist(fn_mass, label='Mass W c.a. QCD', color='orange', histtype='step', bins=bins)
+    fig_2.legend()
+
+    plt.title('Ungroomed mass of jets.')
+    plt.savefig('Pictures/mass_histogram')
+    plt.show()
+
 
 def model_to_val():
-    X_train, _, _, y_train, _, _ = getReadyData()
+    x_train, _, _, y_train, _, _ = getReadyData()
     from keras.models import load_model
     model = load_model('./hand_made_models/128_2_GRU_0.5_sigmoid_adam_0.03_0.09.hdf5')
-    y_hat = model.predict(X_train, verbose=1)
+    y_hat = model.predict(x_train, verbose=1)
 
     y_hat = np.argmax(y_hat, axis=1)
     y_train = np.argmax(y_train, axis=1)
@@ -31,28 +48,32 @@ def model_to_val():
     np.save('false_neg.npy', np.logical_and(y_hat == 0, y_train == 1))
     np.save('true_neg.npy', np.logical_and(y_hat == 0, y_train == 0))
 
-def lund_diffs(isolate = 0):
-    X_train, _, _, _, _, _ = getReadyData()
+
+def lund_diffs(isolate=21):
+    x_train, _, _, _, _, _ = getReadyData()
     tp = np.load('true_pos.npy')
     fp = np.load('false_pos.npy')
     fn = np.load('false_neg.npy')
     tn = np.load('true_neg.npy')
 
-    if (isolate != 0): # advance sort
-        lundPlot(X_train[tp, isolate-1:isolate], 'True Positives (W c.a. W)')
-        lundPlot(X_train[fp, isolate-1:isolate], 'False Positives (QCD c.a. W)')
-        lundPlot(X_train[fn, isolate-1:isolate], 'False Negatives (W c.a. QCD)')
-        lundPlot(X_train[tn, isolate-1:isolate], 'True Negatives (QCD c.a. QCD)')
+    if isolate != 0:  # advance sort
+        lundPlot(x_train[tp, isolate - 1:isolate], 'True Positives (W c.a. W)')
+        lundPlot(x_train[fp, isolate - 1:isolate], 'False Positives (QCD c.a. W)')
+        lundPlot(x_train[fn, isolate - 1:isolate], 'False Negatives (W c.a. QCD)')
+        lundPlot(x_train[tn, isolate - 1:isolate], 'True Negatives (QCD c.a. QCD)')
     else:
-        lundPlot(X_train[tp], 'True Positives (W c.a. W)')
-        lundPlot(X_train[fp], 'False Positives (QCD c.a. W)')
-        lundPlot(X_train[fn], 'False Negatives (W c.a. QCD)')
-        lundPlot(X_train[tn], 'True Negatives (QCD c.a. QCD)')
+        lundPlot(x_train[tp], 'True Positives (W c.a. W)')
+        lundPlot(x_train[fp], 'False Positives (QCD c.a. W)')
+        lundPlot(x_train[fn], 'False Negatives (W c.a. QCD)')
+        lundPlot(x_train[tn], 'True Negatives (QCD c.a. QCD)')
 
     plt.show()
 
+
 def main():
-    lund_diffs()
+    mass_get()
+    # lund_diffs(1)
+
 
 if __name__ == '__main__':
-  main()
+    main()
